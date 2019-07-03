d831a634bfa3a3214c1b79c3c827d0e710267ed7
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-07-04 01:01:37 +0200

    Set up react router and apollo client.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,76 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/styles";
 
-const App = () => {
-  return (
-    <div>
-      Welcome to WCA Live
-    </div>
-  )
-};
+import Navigation from '../Navigation/Navigation';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#63a4ff',
+      main: '#1976d2',
+      dark : '#004ba0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#484848',
+      main: '#212121',
+      dark : '#000000',
+      contrastText: '#fff',
+    },
+  },
+});
+
+/* Apollo configuration */
+
+const httpLink = createHttpLink(
+  process.env.NODE_ENV === 'production'
+    ? { uri: '/api', credentials: 'same-origin' }
+    : { uri: 'http://localhost:4000/api', credentials: 'include' }
+);
+
+const wsLink = new WebSocketLink({
+  uri: process.env.NODE_ENV === 'production' ? `wss://${window.location.host}/api` : 'ws://localhost:4000/api',
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navigation />
+      </ThemeProvider>
+    </ApolloProvider>
+  </BrowserRouter>
+);
 
 export default App;
