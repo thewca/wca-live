1a881ae086903b7f13e131628628c75ecf71f21c
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-07-09 21:12:02 +0200

    Implement round GraphQL endpoint.

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -1,5 +1,6 @@
 const { withAuthentication } = require('./middleware');
 const { ObjectId } = require('mongodb');
+const { parseActivityCode } = require('../utils/wcif');
 
 module.exports = {
   me: async (parent, args, { session, mongo: { Users } }) => {
@@ -8,5 +9,12 @@ module.exports = {
   competition: async (parent, { id }, { mongo: { Competitions } }) => {
     const competition = await Competitions.findOne({ 'wcif.id': id });
     return competition.wcif;
+  },
+  round: async (parent, { competitionId, roundId }, { mongo: { Competitions } }) => {
+    const { eventId } = parseActivityCode(roundId);
+    const competition = await Competitions.findOne({ 'wcif.id': competitionId });
+    const event = competition.wcif.events.find(event => event.id === eventId);
+    const round = event.rounds.find(round => round.id === roundId);
+    return round;
   },
 };
---
+++ server/utils/wcif.js
@@ -0,0 +1,13 @@
+const parseActivityCode = activityCode => {
+  const [, e, r, g, a] = activityCode.match(/(\w+)(?:-r(\d+))?(?:-g(\d+))?(?:-a(\d+))?/);
+  return {
+    eventId: e,
+    roundNumber: r && parseInt(r, 10),
+    groupNumber: g && parseInt(g, 10),
+    attemptNumber: a && parseInt(a, 10)
+  };
+};
+
+module.exports = {
+  parseActivityCode
+};
