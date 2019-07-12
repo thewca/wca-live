643d364d00e167180ea24ba0d627ebfe12c2f174
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-07-13 01:54:35 +0200

    Improve time input.

--- client/src/components/Admin/ResultForm/ResultForm.js
+++ client/src/components/Admin/ResultForm/ResultForm.js
@@ -5,14 +5,47 @@ import TextField from '@material-ui/core/TextField';
 
 import { toInt, setAt, preventDefault, times } from '../../../logic/utils';
 
+const reformatInput = input => {
+  if (input.includes('f') || input.includes('/')) return 'DNF';
+  if (input.includes('s') || input.includes('*')) return 'DNS';
+  const number = toInt(input.replace(/\D/g, '')) || 0;
+  if (number === 0) return '';
+  const str = '00000000' + number.toString().slice(0, 8);
+  return (`${str.slice(-8, -6)}:${str.slice(-6, -4)}:${str.slice(-4, -2)}.${str.slice(-2)}`).replace(/^[0:]*(?!\.)/g, '');
+};
+
+const inputToCentiseconds = input => {
+  if (input === '') return 0;
+  if (input === 'DNF') return -1;
+  if (input === 'DNS') return -2;
+  const num = toInt(input.replace(/\D/g, '')) || 0;
+  return Math.floor(num / 1000000) * 360000 + Math.floor((num % 1000000) / 10000) * 6000 + Math.floor((num % 10000) / 100) * 100 + (num % 100);
+};
+
+const centisecondsToInput = centiseconds => {
+  if (centiseconds === 0) return '';
+  if (centiseconds === -1) return 'DNF';
+  if (centiseconds === -2) return 'DNS';
+  return new Date(centiseconds * 10).toISOString().substr(11, 11).replace(/^[0:]*(?!\.)/g, '');
+};
+
+const normalize = input => centisecondsToInput(inputToCentiseconds(input));
+
 const ResultForm = ({ onSubmit, results, format }) => {
   const { solveCount } = format;
   const [personId, setPersonId] = useState(null);
-  const [attempts, setAttempts] = useState(times(solveCount, () => 0));
+  const [attemptInputs, setAttemptInputs] = useState(times(solveCount, () => ''));
   const result = personId && results.find(result => result.person.id === personId.toString());
 
+  const handleSubmit = preventDefault(() => {
+    onSubmit({
+      personId,
+      attempts: attemptInputs.map(inputToCentiseconds),
+    })
+  });
+
   return (
-    <form onSubmit={preventDefault(() => onSubmit({ personId, attempts }))}>
+    <form onSubmit={handleSubmit}>
       <Grid container spacing={1}>
         <Grid item xs={12} style={{ marginBottom: 16 }}>
           <TextField
@@ -25,19 +58,22 @@ const ResultForm = ({ onSubmit, results, format }) => {
               const personId = toInt(event.target.value);
               const result = personId && results.find(result => result.person.id === personId.toString());
               setPersonId(personId);
-              setAttempts(result ? result.attempts : times(solveCount, () => 0));
+              setAttemptInputs(result ? result.attempts.map(centisecondsToInput) : times(solveCount, () => ''));
             }}
           />
         </Grid>
-        {attempts.map((attempt, index) => (
+        {attemptInputs.map((attemptInput, index) => (
           <Grid item xs={12} key={index}>
             <TextField
+              /* inputProps={{ style: { textAlign: 'right', fontSize: '2em' }}} */
               fullWidth
               variant="outlined"
               label={`Attempt ${index + 1}`}
-              value={attempt || ""}
+              value={attemptInput || ""}
               disabled={!result}
-              onChange={event => setAttempts(setAt(attempts, index, toInt(event.target.value) || 0))}
+              onChange={event => setAttemptInputs(setAt(attemptInputs, index, reformatInput(event.target.value)))}
+              /* onBlur={() => setAttemptInputs(setAt(attemptInputs, index, normalize(attemptInput)))} */
+              error={attemptInput !== normalize(attemptInput)}
             />
           </Grid>
         ))}
