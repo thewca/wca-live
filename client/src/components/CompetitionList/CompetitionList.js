4c6a3229964bca4c4e719698d3a13a8fcc202d57
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-07-23 00:30:59 +0200

    Add country flags.

--- client/src/components/CompetitionList/CompetitionList.js
+++ client/src/components/CompetitionList/CompetitionList.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 
+import FlagIcon from '../FlagIcon/FlagIcon';
 import { formatDateRange } from '../../logic/utils';
 
 const CompetitionList = ({ title, competitions }) => {
   return (
-    <List>
+    <List dense={true}>
       <ListSubheader disableSticky>{title}</ListSubheader>
       {competitions.map(competition => (
         <ListItem
@@ -18,6 +20,9 @@ const CompetitionList = ({ title, competitions }) => {
           component={Link}
           to={`/competitions/${competition.id}`}
         >
+          <ListItemIcon>
+            <FlagIcon code="gb" size="lg" />
+          </ListItemIcon>
           <ListItemText
             primary={competition.name}
             secondary={formatDateRange(competition.startDate, competition.endDate)}
