069bf4917239418e766087bad730f83d22e36baf
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-07-31 01:59:01 +0200

    Don't use cache-first approach for loading data as it leads to data inconsistency.

--- client/src/components/CustomQuery/CustomQuery.js
+++ client/src/components/CustomQuery/CustomQuery.js
@@ -6,7 +6,14 @@ import ErrorSnackbar from '../ErrorSnackbar/ErrorSnackbar';
 
 /* Wrapper around Query with common error and loading logic. */
 const CustomQuery = ({ children, ...props }) => (
-  <Query {...props}>
+  /* Note: network-only means that a query never reads initial data from the cache.
+     This ensures that people always get fresh data. Apollo defaults to cache-first
+     but there are several issues with caching:
+     - event/round/competitor ids are not globally unique, which breaks the cache
+     - some actions introduce many changes to data, which makes updating cache unconvenient
+       (e.g. opening a round clears missing results from previous round, so we'd need to
+       load all the results from previous round in order for the cache to update automatically) */
+  <Query {...props} fetchPolicy="network-only">
     {(...args) => {
       const [{ loading, error }] = args;
       if (error) return <ErrorSnackbar message="Something went wrong 😔" />;
