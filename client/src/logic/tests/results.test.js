5329cd0975487eecc3de046ea525f915cf471bf2
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-08-21 00:50:51 +0200

    Add more tests.

--- client/src/logic/tests/results.test.js
+++ client/src/logic/tests/results.test.js
@@ -1,4 +1,10 @@
-import { formatResult, attemptsWarning } from '../results';
+import {
+  formatResult,
+  encodeMbldResult,
+  validateMbldResult,
+  meetsCutoff,
+  attemptsWarning,
+} from '../results';
 
 describe('formatResult', () => {
   test('returns an empty string for value 0', () => {
@@ -41,6 +47,105 @@ describe('formatResult', () => {
   });
 });
 
+describe('encodeMbldResult', () => {
+  test('correctly encodes DNF, DNS and skipped', () => {
+    expect(encodeMbldResult({ centiseconds: 0 })).toEqual(0);
+    expect(encodeMbldResult({ centiseconds: -1 })).toEqual(-1);
+    expect(encodeMbldResult({ centiseconds: -2 })).toEqual(-2);
+  });
+
+  test('correctly encodes successful result', () => {
+    const result = { solved: 11, attempted: 13, centiseconds: 3480 * 100 };
+    expect(encodeMbldResult(result)).toEqual(900348002);
+  });
+
+  test('rounds centiseconds to seconds', () => {
+    const result = { solved: 11, attempted: 13, centiseconds: 3480 * 100 + 50 };
+    expect(encodeMbldResult(result)).toEqual(900348102);
+  });
+});
+
+describe('validateMbldResult', () => {
+  test('sets attempted to solved when attempted is 0', () => {
+    const result = { solved: 2, attempted: 0, centiseconds: 6000 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 2,
+      attempted: 2,
+      centiseconds: 6000,
+    });
+  });
+
+  test('sets attempted to solved when more cubes are solved than attempted', () => {
+    const result = { solved: 3, attempted: 2, centiseconds: 6000 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 3,
+      attempted: 3,
+      centiseconds: 6000,
+    });
+  });
+
+  test('returns dnf result if the number of points is less than 0', () => {
+    const result = { solved: 2, attempted: 5, centiseconds: 6000 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 0,
+      attempted: 0,
+      centiseconds: -1,
+    });
+  });
+
+  test('returns dnf result for 1/2', () => {
+    const result = { solved: 1, attempted: 2, centiseconds: 6000 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 0,
+      attempted: 0,
+      centiseconds: -1,
+    });
+  });
+
+  test('returns dnf result if the time limit is exceeded', () => {
+    const result = { solved: 2, attempted: 3, centiseconds: 40 * 60 * 100 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 0,
+      attempted: 0,
+      centiseconds: -1,
+    });
+  });
+
+  test('returns the same result if everything is ok', () => {
+    const result = { solved: 11, attempted: 12, centiseconds: 60 * 60 * 100 };
+    expect(validateMbldResult(result)).toEqual({
+      solved: 11,
+      attempted: 12,
+      centiseconds: 60 * 60 * 100,
+    });
+  });
+});
+
+describe('meetsCutoff', () => {
+  it('returns true when no cutoff is given', () => {
+    const attempts = [-1, -1];
+    expect(meetsCutoff(attempts, null)).toEqual(true);
+  });
+
+  it('returns true if one of cutoff results is better than cutoff', () => {
+    const attempts = [1000, 850, 0, 0, 0];
+    const cutoff = { numberOfAttempts: 2, attemptResult: 900 };
+    expect(meetsCutoff(attempts, cutoff)).toEqual(true);
+  });
+
+  it('returns false if one of further results is better than cutoff', () => {
+    const attempts = [1000, 950, 800, 0, 0];
+    const cutoff = { numberOfAttempts: 2, attemptResult: 900 };
+    expect(meetsCutoff(attempts, cutoff)).toEqual(false);
+  });
+
+  it('requires results better than the cutoff', () => {
+    const attempts = [900, 700, 0];
+    const cutoff = { numberOfAttempts: 2, attemptResult: 700 };
+    expect(meetsCutoff(attempts, cutoff)).toEqual(false);
+  });
+});
+
 describe('attemptsWarning', () => {
   const normalize = string => string.replace(/\s+/g, ' ');
 
@@ -62,6 +167,11 @@ describe('attemptsWarning', () => {
 
   it('returns null if results do not look suspicious', () => {
     const attempts = [900, 1000, 800];
+    expect(attemptsWarning(attempts, '333')).toEqual(null);
+  });
+
+  it('does not treat DNF as being far apart from other results', () => {
+    const attempts = [-1, 1000, 2500];
     expect(attemptsWarning(attempts, '333')).toEqual(null);
   });
 });
