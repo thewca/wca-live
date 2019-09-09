c81309f69188fe947400264fb62addce2fcbb1bc
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-09-10 01:46:07 +0200

    Refactor result form navigation.

--- client/src/components/AdminCompetition/ResultForm/ResultForm.js
+++ client/src/components/AdminCompetition/ResultForm/ResultForm.js
@@ -193,8 +193,11 @@ const getInputs = container => {
 const useKeyNavigation = container => {
   useEffect(() => {
     if (!container) return;
-
     const handleKeyPress = event => {
+      if (event.key === 'Esc') {
+        event.target.blur && event.target.blur();
+        return;
+      }
       if (
         ['ArrowUp', 'ArrowDown'].includes(event.key) &&
         container.querySelector('[aria-expanded="true"]')
@@ -202,35 +205,31 @@ const useKeyNavigation = container => {
         /* Don't interrupt navigation within competitor select list. */
         return;
       }
-      if (
-        event.target.tagName === 'INPUT' &&
-        ['ArrowUp', 'ArrowDown', 'Enter'].includes(event.key)
-      ) {
-        /* Blur the current input first, as it may affect which fields are disabled.
-           After that get the updated input list.
-           Note: blur is a synchronous event, so this call triggers all onBlur handlers before continuing. */
+      if (!['ArrowUp', 'ArrowDown', 'Enter'].includes(event.key)) return;
+      event.preventDefault(); /* Prevent page scrolling. */
+      if (event.target.tagName === 'INPUT') {
+        /* Blur the current input first, as it may affect which fields are disabled. */
         event.target.blur();
       }
-      const inputs = getInputs(container);
-      const index = inputs.findIndex(input => event.target === input);
-      if (index === -1) return;
-      const mod = n => (n + inputs.length) % inputs.length;
-      if (event.key === 'ArrowUp') {
-        const previousElement = inputs[mod(index - 1)];
-        previousElement.focus();
-        previousElement.select && previousElement.select();
-        event.preventDefault();
-      } else if (
-        event.key === 'ArrowDown' ||
-        (event.target.tagName === 'INPUT' && event.key === 'Enter')
-      ) {
-        const nextElement = inputs[mod(index + 1)];
-        nextElement.focus();
-        nextElement.select && nextElement.select();
-        event.preventDefault();
-      } else if (event.key === 'Escape') {
-        event.target.blur();
-      }
+      /* Other handlers may change which fields are disabled, so let them run first. */
+      setTimeout(() => {
+        const inputs = getInputs(container);
+        const index = inputs.findIndex(input => event.target === input);
+        if (index === -1) return;
+        const mod = n => (n + inputs.length) % inputs.length;
+        if (event.key === 'ArrowUp') {
+          const previousElement = inputs[mod(index - 1)];
+          previousElement.focus();
+          previousElement.select && previousElement.select();
+        } else if (
+          event.key === 'ArrowDown' ||
+          (event.target.tagName === 'INPUT' && event.key === 'Enter')
+        ) {
+          const nextElement = inputs[mod(index + 1)];
+          nextElement.focus();
+          nextElement.select && nextElement.select();
+        }
+      }, 0);
     };
     container.addEventListener('keydown', handleKeyPress);
     return () => container.removeEventListener('keydown', handleKeyPress);
