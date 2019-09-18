ec9070013445a8ae849b0f1959a165bf8c2ff373
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-09-18 20:04:14 +0200

    Link to WCA profile on the competitor results view.

--- client/src/components/Competitor/Competitor.js
+++ client/src/components/Competitor/Competitor.js
@@ -3,18 +3,22 @@ import gql from 'graphql-tag';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
+import wcaLogo from '../Home/logo.svg';
 import CustomQuery from '../CustomQuery/CustomQuery';
 import FlagIcon from '../FlagIcon/FlagIcon';
 import CompetitorResultsTable from '../CompetitorResultsTable/CompetitorResultsTable';
 import CompetitorResultDialog from '../CompetitorResultDialog/CompetitorResultDialog';
 import { groupBy } from '../../logic/utils';
+import { wcaUrl } from '../../logic/url-utils';
 
 const COMPETITOR_QUERY = gql`
   query Competitor($competitionId: ID!, $competitorId: ID!) {
     competitor(competitionId: $competitionId, competitorId: $competitorId) {
       id
       name
+      wcaId
       country {
         iso2
       }
@@ -45,7 +49,17 @@ const COMPETITOR_QUERY = gql`
   }
 `;
 
+const useStyles = makeStyles(theme => ({
+  competitor: {
+    marginBottom: theme.spacing(2),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
+
 const Competitor = ({ match }) => {
+  const classes = useStyles();
   const { competitionId, competitorId } = match.params;
   const [selectedResult, setSelectedResult] = useState(null);
 
@@ -62,10 +76,34 @@ const Competitor = ({ match }) => {
         );
         return (
           <div>
-            <Typography variant="h5" style={{ marginBottom: 16 }}>
-              {competitor.name}{' '}
-              <FlagIcon code={competitor.country.iso2.toLowerCase()} />
-            </Typography>
+            <Grid
+              container
+              alignContent="center"
+              className={classes.competitor}
+            >
+              <Grid item>
+                <Typography variant="h5">
+                  {competitor.name}{' '}
+                  <FlagIcon code={competitor.country.iso2.toLowerCase()} />
+                </Typography>
+              </Grid>
+              <Grid item className={classes.grow} />
+              {competitor.wcaId && (
+                <Grid item>
+                  <a
+                    href={wcaUrl(`/persons/${competitor.wcaId}`)}
+                    target="_blank"
+                  >
+                    <img
+                      src={wcaLogo}
+                      alt="WCA Profile"
+                      height="32"
+                      width="32"
+                    />
+                  </a>
+                </Grid>
+              )}
+            </Grid>
             <Grid container direction="column" spacing={2}>
               {Object.entries(resultsByEvent).map(([eventName, results]) => (
                 <Grid item key={eventName}>
