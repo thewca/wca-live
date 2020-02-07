d7666dd3fb69426587b207a8ba8aec474e9df077
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-02-07 18:05:00 +0100

    Reverse admin managable competitions and add scroll.

--- client/src/components/admin/AdminCompetitionList/AdminCompetitionList.js
+++ client/src/components/admin/AdminCompetitionList/AdminCompetitionList.js
@@ -7,6 +7,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import { useConfirm } from 'material-ui-confirm';
+import { makeStyles } from '@material-ui/core/styles';
 
 import ErrorSnackbar from '../../ErrorSnackbar/ErrorSnackbar';
 import { formatDateRange } from '../../../logic/date';
@@ -20,11 +21,19 @@ const IMPORT_COMPETITION_MUTATION = gql`
   }
 `;
 
+const useStyles = makeStyles(theme => ({
+  scrollable: {
+    maxHeight: 240,
+    overflowY: 'auto',
+  },
+}));
+
 const AdminCompetitionList = ({
   manageableCompetitions,
   importableCompetitions,
   history,
 }) => {
+  const classes = useStyles();
   const confirm = useConfirm();
   const [importCompetition, { loading, error }] = useMutation(
     IMPORT_COMPETITION_MUTATION,
@@ -38,22 +47,27 @@ const AdminCompetitionList = ({
   return (
     <List dense={true}>
       <ListSubheader disableSticky>Manageable competitions</ListSubheader>
-      {manageableCompetitions.map(competition => (
-        <ListItem
-          key={competition.id}
-          button
-          component={Link}
-          to={`/admin/competitions/${competition.id}`}
-        >
-          <ListItemText
-            primary={competition.name}
-            secondary={formatDateRange(
-              competition.schedule.startDate,
-              competition.schedule.endDate
-            )}
-          />
-        </ListItem>
-      ))}
+      <div className={classes.scrollable}>
+        {manageableCompetitions
+          .slice()
+          .reverse()
+          .map(competition => (
+            <ListItem
+              key={competition.id}
+              button
+              component={Link}
+              to={`/admin/competitions/${competition.id}`}
+            >
+              <ListItemText
+                primary={competition.name}
+                secondary={formatDateRange(
+                  competition.schedule.startDate,
+                  competition.schedule.endDate
+                )}
+              />
+            </ListItem>
+          ))}
+      </div>
       <ListSubheader disableSticky>Importable competitions</ListSubheader>
       {importableCompetitions.map(competition => (
         <ListItem
