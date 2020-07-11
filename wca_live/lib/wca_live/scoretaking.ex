23e9a12252293690d7bf38762494b0e3a0995e1e
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-07-11 19:38:12 +0200

    Clean up ranking/advancing/records computation.

--- wca_live/lib/wca_live/scoretaking.ex
+++ wca_live/lib/wca_live/scoretaking.ex
@@ -42,12 +42,10 @@ defmodule WcaLive.Scoretaking do
   def get_result!(id), do: Repo.get!(Result, id)
 
   def update_result(result, attrs) do
+    result = Repo.preload(result, round: [:competition_event])
+
     Multi.new()
-    |> Multi.run(:result, fn repo, _changes ->
-      result = repo.preload(result, round: [:competition_event])
-      {:ok, result}
-    end)
-    |> Multi.update(:updated_result, fn %{result: result} ->
+    |> Multi.update(:updated_result, fn _changes ->
       format = Format.get_by_id!(result.round.format_id)
       event_id = result.round.competition_event.event_id
       Result.changeset(result, attrs, event_id, format.number_of_attempts)
@@ -55,51 +53,30 @@ defmodule WcaLive.Scoretaking do
     |> Multi.merge(fn %{updated_result: result} ->
       process_round_after_results_change(result.round)
     end)
-    |> Multi.run(:final_result, fn repo, _changes ->
-      result = repo.get!(Result, result.id)
-      {:ok, result}
-    end)
     |> Repo.transaction()
     |> case do
-      {:ok, %{final_result: result}} -> {:ok, result}
-      # TODO: use original error to privde a better message.
-      {:error, _, _, _} -> {:error, "result update failed"}
+      {:ok, _} -> {:ok, Repo.get!(Result, result.id)}
+      {:error, _, reason, _} -> {:error, reason}
     end
   end
 
   defp process_round_after_results_change(round) do
-    competition_event =
-      round
-      |> Ecto.assoc(:competition_event)
-      |> Repo.one!()
-      |> Repo.preload(rounds: [:competition_event, results: [person: [:personal_bests]]])
-
-    round = Enum.find(competition_event.rounds, &(&1.id == round.id))
-
-    ranking_changesets = compute_ranking(round)
-    record_tags_changesets = compute_record_tags(competition_event, round.number)
-    # TODO: advancing depends on ranking ;_;
-    advancing_changesets = compute_advancing(round)
-
-    (ranking_changesets ++ record_tags_changesets ++ advancing_changesets)
-    |> merge_changesets_by_data()
-    |> changesets_to_multi_update()
-  end
-
-  # Groups changesets for the same data and merges them into one.
-  defp merge_changesets_by_data(changesets) do
-    changesets
-    |> Enum.group_by(fn changeset -> changeset.data end)
-    |> Enum.map(fn {_data, changesets} -> Enum.reduce(changesets, &Changeset.merge/2) end)
-  end
-
-  defp changesets_to_multi_update(changesets, name \\ :changeset) do
-    Enum.reduce(changesets, Multi.new(), fn changeset, multi ->
-      Multi.update(multi, {name, changeset.data.id}, changeset)
+    Multi.new()
+    |> Multi.update(:compute_ranking, fn _ ->
+      compute_ranking(round)
+    end)
+    |> Multi.update(:compute_advancing, fn %{compute_ranking: round} ->
+      # Note: advancement usually depends on ranking, that's why we compute it first.
+      compute_advancing(round)
+    end)
+    |> Multi.update(:compute_record_tags, fn %{compute_advancing: round} ->
+      competition_event = round |> Ecto.assoc(:competition_event) |> Repo.one!()
+      compute_record_tags(competition_event)
     end)
   end
 
   defp compute_ranking(round) do
+    round = round |> Repo.preload(:results)
     results = round.results
     format = Format.get_by_id!(round.format_id)
 
@@ -127,6 +104,7 @@ defmodule WcaLive.Scoretaking do
     |> Enum.map(fn {result, ranking} ->
       Changeset.change(result, ranking: ranking)
     end)
+    |> put_results_in_round(round)
   end
 
   defp result_to_monotnic(result, :best = _sort_by) do
@@ -141,7 +119,17 @@ defmodule WcaLive.Scoretaking do
     result_to_monotnic(result1, sort_by) == result_to_monotnic(result2, sort_by)
   end
 
-  defp compute_record_tags(competition_event, changed_round_number) do
+  defp put_results_in_round(results, round) do
+    round |> Changeset.change() |> Changeset.put_assoc(:results, results)
+  end
+
+  # Updating a result with record may affect subsequent round results,
+  # so we compute proper record tags for all rounds of the given event.
+  defp compute_record_tags(competition_event) do
+    competition_event =
+      competition_event
+      |> Repo.preload(rounds: [:competition_event, results: [person: [:personal_bests]]])
+
     rounds = competition_event.rounds
     event_id = competition_event.event_id
 
@@ -155,46 +143,38 @@ defmodule WcaLive.Scoretaking do
 
     records = merge_records(regional_records, personal_records)
 
-    {previous_rounds, affected_rounds} =
-      Enum.split_with(rounds, fn round ->
-        round.number < changed_round_number
+    {round_changesets, _records} =
+      Enum.map_reduce(rounds, records, fn round, records ->
+        records = round |> round_records() |> merge_records(records)
+
+        round_changeset =
+          round.results
+          |> Enum.map(fn result ->
+            single_record_tag =
+              tags_with_record_key(result.person, event_id, "single")
+              |> Enum.find_value(fn %{record_key: record_key, tag: tag} ->
+                if result.best == records[record_key], do: tag, else: nil
+              end)
+
+            average_record_tag =
+              tags_with_record_key(result.person, event_id, "average")
+              |> Enum.find_value(fn %{record_key: record_key, tag: tag} ->
+                if result.average == records[record_key], do: tag, else: nil
+              end)
+
+            Changeset.change(result,
+              single_record_tag: single_record_tag,
+              average_record_tag: average_record_tag
+            )
+          end)
+          |> put_results_in_round(round)
+
+        {round_changeset, records}
       end)
 
-    records =
-      previous_rounds
-      |> Enum.map(&round_records/1)
-      |> Enum.reduce(%{}, &merge_records/2)
-      |> merge_records(records)
-
-    affected_rounds
-    |> Enum.reduce({records, []}, fn round, {records, changesets} ->
-      records = round |> round_records() |> merge_records(records)
-
-      round_changesets =
-        Enum.map(round.results, fn result ->
-          single_record_tag =
-            tags_with_record_key(result.person, event_id, "single")
-            |> Enum.find_value(fn %{record_key: record_key, tag: tag} ->
-              if result.best == records[record_key], do: tag, else: nil
-            end)
-
-          average_record_tag =
-            tags_with_record_key(result.person, event_id, "average")
-            |> Enum.find_value(fn %{record_key: record_key, tag: tag} ->
-              if result.average == records[record_key], do: tag, else: nil
-            end)
-
-          Changeset.change(result,
-            single_record_tag: single_record_tag,
-            average_record_tag: average_record_tag
-          )
-        end)
-
-      {records, round_changesets ++ changesets}
-    end)
-    |> case do
-      {_, changesets} -> changesets
-    end
+    competition_event
+    |> Changeset.change()
+    |> Changeset.put_assoc(:rounds, round_changesets)
   end
 
   defp tags_with_record_key(person, event_id, type) do
@@ -257,11 +237,86 @@ defmodule WcaLive.Scoretaking do
   #   - **advancing** - actually being in the next round (the "green" results)
   #   - **qualifying** - satisfying advancement criteria
   defp compute_advancing(round) do
+    round = round |> Repo.preload(:results)
     advancing = advancing_results(round)
 
     Enum.map(round.results, fn result ->
       Changeset.change(result, advancing: result in advancing)
     end)
+    |> put_results_in_round(round)
+  end
+
+  defp advancing_results(round) do
+    next = get_next_round(round) |> Repo.preload(:results)
+
+    if next != nil and Round.open?(next) do
+      # If the next round is open use its results to determine who advanced.
+      advancing_person_ids = Enum.map(next.results, & &1.person_id)
+
+      Enum.filter(round.results, fn result ->
+        result.person_id in advancing_person_ids
+      end)
+    else
+      format = Format.get_by_id!(round.format_id)
+      qualifying_results(round.results, round.advancement_condition, format)
+    end
+  end
+
+  defp qualifying_results([], _advancement_condition, _format), do: []
+
+  defp qualifying_results(results, nil = _advancement_condition, _format) do
+    # Mark top 3 in the finals (unless DNFed).
+    Enum.filter(results, fn result ->
+      result.best > 0 and result.ranking != nil and result.ranking <= 3
+    end)
+  end
+
+  defp qualifying_results(results, advancement_condition, format) do
+    # See: https://www.worldcubeassociation.org/regulations/#9p1
+    max_qualifying = floor(length(results) * 0.75)
+    rankings = results |> Enum.map(& &1.ranking) |> Enum.reject(&is_nil/1) |> Enum.sort()
+
+    first_non_qualifying_ranking =
+      if length(rankings) > max_qualifying do
+        Enum.fetch!(rankings, max_qualifying)
+      else
+        if Enum.empty?(rankings), do: 1, else: List.last(rankings) + 1
+      end
+
+    Enum.filter(results, fn result ->
+      # Note: this ensures that people who tied either qualify together or not.
+      result.ranking != nil and
+        result.ranking < first_non_qualifying_ranking and
+        result.best > 0 and
+        satisfies_advancement_condition?(result, advancement_condition, length(results), format)
+    end)
+  end
+
+  defp satisfies_advancement_condition?(
+         result,
+         %AdvancementCondition{type: "ranking", level: level},
+         _total_results,
+         _format
+       ) do
+    result.ranking <= level
+  end
+
+  defp satisfies_advancement_condition?(
+         result,
+         %AdvancementCondition{type: "percent", level: level},
+         total_results,
+         _format
+       ) do
+    result.ranking <= floor(total_results * level * 0.01)
+  end
+
+  defp satisfies_advancement_condition?(
+         result,
+         %AdvancementCondition{type: "attemptResult", level: level},
+         _total_results,
+         format
+       ) do
+    AttemptResult.better?(Map.get(result, format.sort_by), level)
   end
 
   def open_round(round) do
@@ -356,79 +411,6 @@ defmodule WcaLive.Scoretaking do
 
   defp accepted?(%Person{registration: %{status: "accepted"}}), do: true
   defp accepted?(_person), do: false
-
-  defp advancing_results(round) do
-    next = get_next_round(round) |> Repo.preload(:results)
-
-    if next != nil and Round.open?(next) do
-      # If the next round is open use its results to determine who advanced.
-      advancing_person_ids = Enum.map(next.results, & &1.person_id)
-
-      Enum.filter(round.results, fn result ->
-        result.person_id in advancing_person_ids
-      end)
-    else
-      format = Format.get_by_id!(round.format_id)
-      qualifying_results(round.results, round.advancement_condition, format)
-    end
-  end
-
-  defp qualifying_results([], _advancement_condition, _format), do: []
-
-  defp qualifying_results(results, nil = _advancement_condition, _format) do
-    # Mark top 3 in the finals (unless DNFed).
-    Enum.filter(results, fn result ->
-      result.best > 0 and result.ranking != nil and result.ranking <= 3
-    end)
-  end
-
-  defp qualifying_results(results, advancement_condition, format) do
-    # See: https://www.worldcubeassociation.org/regulations/#9p1
-    max_qualifying = floor(length(results) * 0.75)
-    rankings = results |> Enum.map(& &1.ranking) |> Enum.reject(&is_nil/1) |> Enum.sort()
-
-    first_non_qualifying_ranking =
-      if length(rankings) > max_qualifying do
-        Enum.fetch!(rankings, max_qualifying)
-      else
-        if Enum.empty?(rankings), do: 1, else: List.last(rankings) + 1
-      end
-
-    Enum.filter(results, fn result ->
-      # Note: this ensures that people who tied either qualify together or not.
-      result.ranking != nil and
-        result.ranking < first_non_qualifying_ranking and
-        result.best > 0 and
-        satisfies_advancement_condition?(result, advancement_condition, length(results), format)
-    end)
-  end
-
-  defp satisfies_advancement_condition?(
-         result,
-         %AdvancementCondition{type: "ranking", level: level},
-         _total_results,
-         _format
-       ) do
-    result.ranking <= level
-  end
-
-  defp satisfies_advancement_condition?(
-         result,
-         %AdvancementCondition{type: "percent", level: level},
-         total_results,
-         _format
-       ) do
-    result.ranking <= floor(total_results * level * 0.01)
-  end
-
-  defp satisfies_advancement_condition?(
-         result,
-         %AdvancementCondition{type: "attemptResult", level: level},
-         _total_results,
-         format
-       ) do
-    AttemptResult.better?(Map.get(result, format.sort_by), level)
-  end
 
   def clear_round(round) do
     round = round |> Repo.preload(:results)
