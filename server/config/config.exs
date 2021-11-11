9ebe4142835bed08acb7db775e4fcb2b4d6918bf
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-11-11 01:38:38 +0100

    Fix deprecations

--- server/config/config.exs
+++ server/config/config.exs
@@ -1,12 +1,4 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
-
-use Mix.Config
+import Config
 
 config :wca_live,
   ecto_repos: [WcaLive.Repo]
