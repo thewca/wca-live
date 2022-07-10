c3ebdf2a1138afb0358a49ecaa988365e408d19f
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-07-10 11:06:25 +0200

    Lower slow query threshold

--- server/lib/wca_live/telemetry.ex
+++ server/lib/wca_live/telemetry.ex
@@ -1,10 +1,12 @@
 defmodule WcaLive.Telemetry do
   require Logger
 
+  @slow_query_threshold_ms 800
+
   def handle_event([:wca_live, :repo, :query], measurements, metadata, _config) do
     query_time_ms = System.convert_time_unit(measurements.query_time, :native, :millisecond)
 
-    if query_time_ms > 2_000 do
+    if query_time_ms > @slow_query_threshold_ms do
       Logger.info([
         "SLOW QUERY ",
         Integer.to_string(query_time_ms),
