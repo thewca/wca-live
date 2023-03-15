3a5d2de82fdfc02977447cf446cf9b50f5c3836e
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-03-15 01:12:08 +0100

    Fix results order in round pdf

--- lib/wca_live_web/controllers/pdf_controller.ex
+++ lib/wca_live_web/controllers/pdf_controller.ex
@@ -1,15 +1,22 @@
 defmodule WcaLiveWeb.PdfController do
   use WcaLiveWeb, :controller
 
+  import Ecto.Query
+
   alias WcaLive.Scoretaking
+  alias WcaLive.Scoretaking.Result
   alias WcaLive.Wca
 
   def round(conn, %{"id" => id}) do
-    round = Scoretaking.get_round!(id, results: :person, competition_event: :competition)
+    round =
+      Scoretaking.get_round!(id,
+        results: Result |> Result.order_by_ranking() |> preload([:person]),
+        competition_event: :competition
+      )
 
     event = Wca.Event.get_by_id!(round.competition_event.event_id)
     format = Wca.Format.get_by_id!(round.format_id)
-    stats = Scoretaking.Result.ordered_result_stats(event.id, format)
+    stats = Result.ordered_result_stats(event.id, format)
 
     conn
     |> render("round.pdf", round: round, event: event, format: format, stats: stats)
