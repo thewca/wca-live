5a99d6838256217cc28579275dd50994c2e91339
Cailyn Hoover <choover11@gmail.com>
2024-12-01 21:46:17 -0800

    move test to own describe

--- test/wca_live_web/controllers/competition_controller_test.exs
+++ test/wca_live_web/controllers/competition_controller_test.exs
@@ -17,6 +17,27 @@ defmodule WcaLiveWeb.CompetitionControllerTest do
       assert %{"formatVersion" => "1.0", "id" => "WC2019"} = body
     end
 
+    test "returns error when not signed in", %{conn: conn} do
+      competition = insert(:competition, wca_id: "WC2019")
+
+      conn = get(conn, "/api/competitions/#{competition.id}/wcif")
+
+      body = json_response(conn, 403)
+      assert body == %{"error" => "access denied"}
+    end
+
+    @tag :signed_in
+    test "returns error when not authorized", %{conn: conn} do
+      competition = insert(:competition, wca_id: "WC2019")
+
+      conn = get(conn, "/api/competitions/#{competition.id}/wcif")
+
+      body = json_response(conn, 403)
+      assert body == %{"error" => "access denied"}
+    end
+  end
+
+  describe "show_results" do
     test "returns results for a competition", %{conn: conn} do
       competition = insert(:competition, wca_id: "WC2019")
       competition_event = insert(:competition_event, competition: competition)
@@ -58,25 +79,6 @@ defmodule WcaLiveWeb.CompetitionControllerTest do
                  }
                ]
              }
-    end
-
-    test "returns error when not signed in", %{conn: conn} do
-      competition = insert(:competition, wca_id: "WC2019")
-
-      conn = get(conn, "/api/competitions/#{competition.id}/wcif")
-
-      body = json_response(conn, 403)
-      assert body == %{"error" => "access denied"}
-    end
-
-    @tag :signed_in
-    test "returns error when not authorized", %{conn: conn} do
-      competition = insert(:competition, wca_id: "WC2019")
-
-      conn = get(conn, "/api/competitions/#{competition.id}/wcif")
-
-      body = json_response(conn, 403)
-      assert body == %{"error" => "access denied"}
     end
   end
 
