54e622fd6f3fc0d8aeed07a22605cb88ddfa86e4
Kevin Hays <36387501+HaysCubing@users.noreply.github.com>
2024-12-31 19:26:12 -0800

    updates

--- client/src/components/ResultStat/ResultStat.jsx
+++ client/src/components/ResultStat/ResultStat.jsx
@@ -3,7 +3,7 @@ import {
   bestPossibleAverage,
   worstPossibleAverage,
   formatAttemptResult,
-  average,
+  incompleteMean,
 } from "../../lib/attempt-result";
 import { shouldComputeAverage } from "../../lib/result";
 
@@ -15,6 +15,7 @@ function ResultStat({ result, field, eventId, format, forecastView
     shouldComputeAverage(eventId, format.numberOfAttempts)
   ) {
     const attemptResults = result.attempts.map((attempt) => attempt.result);
+  
     if (format.numberOfAttempts === 5 && result.attempts.length === 4) {
       return (
         <Box component="span" sx={{ opacity: 0.5 }}>
@@ -58,6 +59,20 @@ function ResultStat({ result, field, eventId, format, forecastView
             <span>
               {formatAttemptResult(
                 result.projectedAverage,
+                eventId
+              )}
+            </span>
+          </Tooltip>
+        </Box>
+      );
+    }
+    if (format.numberOfAttempts === 3 && result.attempts.length === 2) {
+      return (
+        <Box component="span" sx={{ opacity: 0.5 }}>
+          <Tooltip title="Mean after 2 solves">
+            <span>
+              {formatAttemptResult(
+                incompleteMean(attemptResults, eventId),
                 eventId
               )}
             </span>
