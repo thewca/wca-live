3f47fdfe7c6519f773f94a68355a988fa5fec0e7
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-04-01 00:32:43 +0900

    Enable New Relic logs

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,8 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+# New Relic
+config :new_relic_agent, logs_in_context: :forwarder
+
 # See `releases.exs` for the remaining configuration
 # that depends on environment variables.
